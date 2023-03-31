3dc96661ab5e1c269560e39615547fbd75d86c2a
Hossam Ahmed Fahmy <93485412+Hossamfahmy2022@users.noreply.github.com>
2023-03-31 14:27:13 +0200

    ass_2

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BirthdayCard());
+}
+
+class BirthdayCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Birthday Card',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Birthday Card'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('images/birthday_image.png'),
+              SizedBox(height: 20),
+              Text(
+                'Happy Birthday!!',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
